aa3fd933a1c68b31972ea4ad14e7ddc8cd671bc9
Aanika-Sadana <as3782@njit.edu>
2024-12-13 11:24:57 -0500

    now deletes duplicate companies

--- automation/find_duplicates.py
+++ automation/find_duplicates.py
@@ -8,7 +8,7 @@ import pandas as pd
 # py -m pip install xlrd
 
 intro_calls_file = pd.read_excel(f"{os.path.dirname(os.path.abspath(__file__))}/../Intro Calls.xlsx")
-companies = [company for company in intro_calls_file["COmpany Name"]]
+companies = [company for company in intro_calls_file["Company Name"]]
 if(not os.path.exists(f"{os.path.dirname(os.path.abspath(__file__))}/../DUPLICATES.csv")):
     with open(f"{os.path.dirname(os.path.abspath(__file__))}/../DUPLICATES.csv", "a+", newline="") as dup_file:
         writer = csv.writer(dup_file)
@@ -20,7 +20,13 @@ writer = csv.writer(duplicate_file)
 for filename in os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}/.."):
         if(filename.endswith(".csv") and "DUPLICATES" not in filename):
             batch = pd.read_csv(f"{os.path.dirname(os.path.abspath(__file__))}/../{filename}")
+            batch_copy = batch.copy(deep=True)
             for organization in batch["Organization"]:
-                if organization in companies and organization not in dup_companies:
-                    writer.writerow([intro_calls_file["Date of Intro Call"][companies.index(organization)].date(), organization, filename.strip(".csv"), date.today()])
-                    dup_companies.append(organization)
+                if organization in companies:
+                    print(f"removing {organization} from {filename.strip(".csv")}")
+                    batch_copy = batch_copy[batch_copy['Organization'] != organization]
+                    if organization not in dup_companies:
+                        writer.writerow([intro_calls_file["Date of Intro Call"][companies.index(organization)].date(), organization, filename.strip(".csv"), date.today()])
+                        dup_companies.append(organization)
+            batch_copy.to_csv(f"{os.path.dirname(os.path.abspath(__file__))}/../{filename}", index=False)
+
